5ecedac059cecc1c20c97e86f58fd1581989b01e
VictorAJ01 <ajcreativity471@gmail.com>
2025-12-10 08:49:34 +0100

    Add: General information tab

--- src/pages/profile/components/recent_device_login.tsx
+++ src/pages/profile/components/recent_device_login.tsx
@@ -3,8 +3,8 @@ import { MdEmail } from "react-icons/md";
 export default function RecentDeviceLogin() {
   return (
     <div className="flex items-center gap-3">
-      <div className="w-12 h-12 bg-light-primary-bg rounded-full flex items-center justify-center">
-        <MdEmail className="text-2xl text-primary" />
+      <div className="w-10 h-10 bg-light-primary-bg rounded-full flex items-center justify-center">
+        <MdEmail className="text-xl text-primary" />
       </div>
       <div className="space-y-1 text-sm">
         <p>10.222.202.13</p>
