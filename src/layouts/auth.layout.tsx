60055a8e81a038ffa4fc40e46d1a83618d7d59ca
VictorAJ01 <ajcreativity471@gmail.com>
2026-01-13 14:50:45 +0100

    Firebase deployment

--- src/layouts/auth.layout.tsx
+++ src/layouts/auth.layout.tsx
@@ -1,14 +1,10 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Image } from "@heroui/react";
 
 import AuthBg from "@/assets/auth-bg.jpg";
 import Logo from "@/assets/logo.png";
-import { authRoutes } from "@/routes";
 
 export default function AuthLayout() {
-  const { pathname } = useLocation();
-  const hideLogo = pathname === authRoutes.verifyOTP;
-
   return (
     <section
       className="w-full min-h-screen p-4 flex items-center justify-center bg-cover bg-center bg-no-repeat"
@@ -18,9 +14,7 @@ export default function AuthLayout() {
     >
       <main className="w-full max-w-2xl h-auto flex items-center justify-center">
         <div className="bg-light-primary-bg  p-8 w-full rounded-2xl shadow">
-          {!hideLogo && (
-            <Image alt="Logo" className="w-25 h-25 object-contain" src={Logo} />
-          )}
+          <Image alt="Logo" className="w-25 h-25 object-contain" src={Logo} />
           <Outlet />
         </div>
       </main>
